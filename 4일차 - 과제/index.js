d42d8c1c7cf10c0eb4c1fa6966c65cb71fd6eb5f
YunheeAhn <ayh95944674@gmail.com>
2025-09-05 00:56:45 +0900

    fix code

--- 4일차 - 과제/index.js
+++ 4일차 - 과제/index.js
@@ -48,7 +48,7 @@ function check369() {
 function checkPrime() {
     const primeNum = Number(document.getElementById("primeInput").value);
     
-    let isPrime = "소수가 아닙니다";
+    let isPrime = "소수 입니다";
 
     if(primeNum <= 1 ) {
         alert(primeNum + "는 " + isPrime)
@@ -57,7 +57,7 @@ function checkPrime() {
     } else {
         for(let i=2; i < primeNum; i++){
             if(primeNum % 1 == 0){
-                isPrime =  "소수입니다" 
+                isPrime =  "소수가 아닙니다" 
             } 
         }
     }
